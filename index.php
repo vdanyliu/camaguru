1ef90b27c73d34415e0dd8a20ced1543395c90c5
vdanyliu <fretalk@ukr.net>
2019-09-13 21:03:34 +0300

    add css text

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <title>MyGallery42</title>
+	<link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
     <header class="header">
-
+		<ul class="menu">
+			<li class="logo">
+				<a href="#">Logo</a>
+			</li>
+			<li>
+				<a href="#">Main</a>
+			</li>
+			<li>
+				<a href="#">Gallery</a>
+			</li>
+			<li>
+				<a href="#">smth</a>
+			</li>
+		</ul>
+		<form action="authorisation.php" method="get">
+			<input type="email" name="email" value="email">
+			<input type="password" name="password" value="">
+			<input type="submit" value="Submit">
+		</form>
     </header>
     <div class="">
 
