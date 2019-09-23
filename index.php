d4318ab2d707ca9ebae1acb355300414cf520237
vdanyliu <fretalk@ukr.net>
2019-09-23 16:10:36 +0300

    add some stuff

--- index.php
+++ index.php
@@ -1,39 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>MyGallery42</title>
-	<link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="css/main.css">
-</head>
-
-<body>
-    <header class="header">
-		<ul class="menu">
-			<li class="logo">
-				<a href="#">Logo</a>
-			</li>
-			<li>
-				<a href="#">Main</a>
-			</li>
-			<li>
-				<a href="#">Gallery</a>
-			</li>
-			<li>
-				<a href="#">smth</a>
-			</li>
-		</ul>
-		<form action="authorisation.php" method="get">
-			<input type="email" name="email" value="email">
-			<input type="password" name="password" value="">
-			<input type="submit" value="Submit">
-		</form>
-    </header>
-    <div class="">
-
-    </div>
-    <footer class="footer">
-
-    </footer>
-</body>
-
-</html>
+<?php
+	require './lib/Dev.php';
+    $test = 'lollll';
+    $test1 = [1, 2, 3, 4];
+	debug($test);
+	debug($test);
+	debug($test1);
+    //var_dump($test1);
+    echo "Lol";
