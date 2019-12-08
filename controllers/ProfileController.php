77eec0b7d4d0dc53dffd8f68143dad5ae4f10c9d
vdanyliu <fretalk@ukr.net>
2019-12-08 22:36:14 +0200

    add unicid to ing

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -11,6 +11,23 @@
 		{
 			$this->model->check();
 			$this->view->render("addPhoto");
+			if (isset($_POST) && isset($_FILES['imageFromForm']))
+			{
+				$fileName = $_FILES['imageFromForm']['name'];
+				$filetmp = $_FILES['imageFromForm']['tmp_name'];
+				$fileExt = explode('.', $fileName);
+				$fileActualExt = strtolower(end($fileExt));
+				if($filetmp != NULL && exif_imagetype($filetmp))
+				{
+					move_uploaded_file($filetmp, "img/" . uniqid("", true) . "." . $fileActualExt);
+					foreach ($_POST as $key => $value)
+						echo $key . "=>" . $value . "<br>";
+					foreach ($_FILES['imageFromForm'] as $key => $value)
+						echo $key . "=>" . $value . "<br>";
+					//var_dump(exif_imagetype("img/" . $fileName));
+				}
+				//var_dump(file("img/" . $fileName));
+			}
 		}
 
 		public function settingsAction()
