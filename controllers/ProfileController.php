e71de7b83f6e96b6ea369c3f275c557c0e7ce3ec
vdanyliu <fretalk@ukr.net>
2019-12-10 22:58:50 +0200

    stiil locking for img alpha save

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -13,26 +13,13 @@
 			$this->view->render("addPhoto");
 			if (isset($_POST) && isset($_FILES['imageFromForm']))
 			{
-				$fileName = $_FILES['imageFromForm']['name'];
-				$filetmp = $_FILES['imageFromForm']['tmp_name'];
-				$fileExt = explode('.', $fileName);
-				$fileActualExt = strtolower(end($fileExt));
-				if($filetmp != NULL && exif_imagetype($filetmp))
-				{
-					move_uploaded_file($filetmp, "img/" . uniqid("", true) . "." . $fileActualExt);
-					foreach ($_POST as $key => $value)
-						echo $key . "=>" . $value . "<br>";
-					foreach ($_FILES['imageFromForm'] as $key => $value)
-						echo $key . "=>" . $value . "<br>";
-					//var_dump(exif_imagetype("img/" . $fileName));
-				}
-				//var_dump(file("img/" . $fileName));
+				$this->model->addPhotoFromPost();
 			}
 		}
 
 		public function settingsAction()
 		{
-			$this->model->check();
+			//$this->model->check();
 			$this->view->render("Settings");
 		}
 	}
--- models/Profile.php
+++ models/Profile.php
@@ -10,4 +10,39 @@ class Profile extends Model
 	{
 		echo "rabotaet";
 	}
+
+	public function addPhotoFromPost()
+	{
+		$fileName = $_FILES['imageFromForm']['name'];
+		$filetmp = $_FILES['imageFromForm']['tmp_name'];
+		$fileExt = explode('.', $fileName);
+		$fileActualExt = strtolower(end($fileExt));
+		if($filetmp != NULL && exif_imagetype($filetmp))
+		{
+			$newfilename = "img/" . uniqid("", true) . "." . $fileActualExt;
+			move_uploaded_file($filetmp, $newfilename);
+			foreach ($_POST as $key => $value)
+				echo $key . "=>" . $value . "<br>";
+			foreach ($_FILES['imageFromForm'] as $key => $value)
+				echo $key . "=>" . $value . "<br>";
+			echo $newfilename . "<br>";
+			$this->mereWithLogo($newfilename);
+		}
+	}
+
+	public function mereWithLogo($imgDest)
+	{
+		$idImg = imagecreatefromjpeg($imgDest);
+		$idLogo = imagecreatefrompng("img/maxresdefault111.png");
+		imagealphablending($idImg, false);
+		imagesavealpha($idImg, true);
+		//imagetruecolortopalette($idImg, true, 128);
+		imagealphablending($idLogo, false);
+		imagesavealpha($idLogo, true);
+
+		//imagecopymerge($idImg, $idLogo, 500, 200, 300, 300, 300, 300, 100);
+		$resultimage = "img/122.jpg";
+		imagepng($idImg, $resultimage);
+		echo "<img src=".$resultimage.">";
+	}
 }
