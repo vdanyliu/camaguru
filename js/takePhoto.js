8a46e0dbd74de9abd0119525b7c628cb012e408f
Volodymyr DANYLIUK <vdanyliu@e1r3p10.unit.ua>
2020-01-15 19:59:52 +0200

    add forbiddenimage array

--- js/takePhoto.js
+++ js/takePhoto.js
@@ -37,7 +37,51 @@
         };
         xhr.send(data);
     });
-
+ //
+//  var video = document.getElementById('video');
+//  // Get access to the camera!
+//  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+//      // Not adding `{ audio: true }` since we only want video now
+//      navigator.mediaDevices.getUserMedia({video: true}).then(function (stream) {
+//          //video.src = window.URL.createObjectURL(stream);
+//          video.srcObject = stream;
+//          video.play();
+//      });
+//  }
+//  var canvas = document.getElementById('canvas');
+//  var context = canvas.getContext('2d');
+//  var video2 = document.getElementById('video');
+// // video.hidden = true;
+// // canvas.hidden = true;
+//
+//  // Trigger photo take
+// // document.getElementById("snap").addEventListener("click", function () {
+//      //context.drawImage(video2, 0, 0, 640, 480);
+//  function videoToImage() {
+//      let canvas = document.getElementById('canvas');
+//      let context = canvas.getContext('2d');
+//      let video2 = document.getElementById('video');
+//
+//      //document.getElementById('canvas').style.display = 'none';
+//      context.drawImage(video2, 0, 0);
+//      let image = new Image();
+//      image.src = canvas.toDataURL("image/png");
+//      let data = new FormData();
+//      data.append('imageSrc', image.src);
+//      let xhr = new XMLHttpRequest();
+//      xhr.open('POST', 'JS/request', true);
+//      xhr.onload = function () {
+//          console.log(this.responseText);
+//          //document.body.innerHTML = "";
+//          //document.write(this.responseText);
+//          let photo = document.getElementById('photo');
+//          photo.innerHTML = this.responseText;
+//      };
+//      xhr.send(data);
+//  }
+//  setInterval(videoToImage, 500);
+ //});
+//---------------------------------------------------------------
     document.getElementById("download").addEventListener('click', function () {
         let canvas = document.getElementById('canvas');
         let context = canvas.getContext('2d');
--- models/JS.php
+++ models/JS.php
@@ -9,16 +9,44 @@ class JS extends Model
 {
 	public function imageSrc()
 	{
-		$imgDest = $_POST['imageSrc'];
 
-		if (exif_imagetype($imgDest)) {
+		$forbiddenImages = [
+			false => false,
+			IMAGETYPE_GIF => true,
+			IMAGETYPE_JPEG => true,
+			IMAGETYPE_PNG => true,
+			IMAGETYPE_SWF => false,
+			IMAGETYPE_PSD => false,
+			IMAGETYPE_BMP => false,
+			IMAGETYPE_TIFF_II => false,
+			IMAGETYPE_TIFF_MM => false,
+			IMAGETYPE_JPC => false,
+			IMAGETYPE_JP2 => false,
+			IMAGETYPE_JPX => false,
+			IMAGETYPE_JB2 => false,
+			IMAGETYPE_SWC => false,
+			IMAGETYPE_IFF => false,
+			IMAGETYPE_WBMP => false,
+			IMAGETYPE_XBM => false,
+			IMAGETYPE_ICO => false,
+			IMAGETYPE_WEBP => false
+		];
+
+		$imgDest = $_POST['imageSrc'];
+		var_dump(exif_imagetype($imgDest));
+
+		if ($forbiddenImages[exif_imagetype($imgDest)]) {
 			$ex = explode(',', $imgDest);
 			$textData = base64_decode($ex[1]);
-			if (!$idImg = @imagecreatefromstring($textData)) {
+			if (!$idImg = imagecreatefromstring($textData)) {
 				echo "wrong image data";
 				exit (0);
 			}
 		}
+			else {
+				echo "wrong image data";
+				exit (0);
+			}
 		$idLogo = imagecreatefrompng("img/cat2.png");
 		imagealphablending($idLogo, true);
 		imagesavealpha($idLogo, true);
@@ -29,7 +57,6 @@ class JS extends Model
 //		$g = ($rgb >> 8) & 0xFF;
 //		$b = $rgb & 0xFF;
 		$blind = 2147483647; //white color
-		var_dump($blind);
 		imagecolortransparent($idLogo, $blind);
 
 		imagecopymerge($idImg, $idLogo, 100, 100, 0, 0, 514, 428, 100);
